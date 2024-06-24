3e4a90472ede659886222db3111506c3aa18083b
Rory Neill <112756114+roryneill07@users.noreply.github.com>
2024-06-24 18:53:04 +1200

    Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,20 @@ module.exports = {
     preflight: false,
   },
   theme: {
+    colors: {
+      'primary': '#fdb819',
+      'secondary': '#212121',
+      'white': '#FFFFFF',
+      'dark-grey': '#282828',
+      'grey': '#D1D2D3'
+
+    },
+    fontFamily: {
+      sans: 'Rubik, sans-serif',
+    },
+    container: {
+      center: true,
+    },
     extend: {},
   },
   plugins: [],
